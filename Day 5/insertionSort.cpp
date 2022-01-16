50c3af5fbe083a81023749868bd99a3021652943
pahujajayesh <jayesh.pahuja123@gamil.com>
2022-01-16 23:25:47 +0530

    Day 5

---
+++ Day 5/insertionSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+void insertionSort(int *arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j;
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = temp;
+    }
+    for (int i = 0; i < n; i++)
+
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    insertionSort(arr, n);
+    return 0;
+}
