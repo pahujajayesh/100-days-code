895e081f9564c616b83a479b35b93635c12eff01
pahujajayesh <jayesh.pahuja123@gamil.com>
2022-02-06 22:08:05 +0530

    Day 24

---
+++ Stack/implementationusingArray.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+class Stack
+{
+public:
+    int *arr;
+    int top;
+    int size;
+
+    Stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+    void push(int element)
+    {
+        if (size - top > 1)
+        {
+            top++;
+            arr[top] = element;
+        }
+        else
+        {
+            cout << "stack overflow" << endl;
+        }
+    }
+    void pop()
+    {
+        if (top >= 0)
+        {
+            top--;
+        }
+        else
+        {
+            cout << "stack underflow" << endl;
+        }
+    }
+    int peek()
+    {
+        if (top >= 0)
+        {
+            return arr[top];
+        }
+        else
+        {
+            cout << "stack is empty" << endl;
+            return -1;
+        }
+    }
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+int main()
+{
+    Stack s(3);
+    s.push(1);
+    s.push(2);
+    s.push(4);
+    s.push(10);
+
+    cout << s.peek() << endl;
+    s.pop();
+    s.pop();
+    cout << s.peek() << endl;
+    cout << s.isEmpty() << endl;
+    return 0;
+}
